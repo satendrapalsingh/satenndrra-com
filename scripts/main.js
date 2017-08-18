97a04bfd21db9b4cbcd777c2cd9581f516e526e0
satenndrra <satenndrra@gmail.com>
2017-08-18 10:28:04 +0000

    typewriter.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,29 @@
-// main javascript code
+// main javscript code
+
+// typewriter.js function reference: https://safi.me.uk/typewriterjs/
+
+$(function(){
+
+    var typed = document.getElementById('typed');
+    
+    var typewriter = new Typewriter(typed, {
+        loop: true,
+        blinkSpeed: 10,
+        typingSpeed: 200
+    });
+    
+    
+    typewriter
+        .typeString("Essential Tech Skills for Entrepreneurs")
+        .pauseFor(2500)
+        .deleteChars(13)
+        .typeString('Solopreneurs')
+        .pauseFor(2500)
+        .deleteChars(12)
+        .typeString("Startup-Founders")
+        .start()
+});
+
+
+
+
