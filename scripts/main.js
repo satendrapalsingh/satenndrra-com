88964a4d1a821930fc4adb1f9b62cac17bfe6db0
satenndrra <satenndrra@gmail.com>
2017-08-19 09:53:00 +0000

    finalised section colors

--- scripts/main.js
+++ scripts/main.js
@@ -11,17 +11,17 @@ $(function(){
         blinkSpeed: 10,
         typingSpeed: 200
     });
-    
-    
-    typewriter
-        .typeString("Essential Tech Skills for Entrepreneurs")
-        .pauseFor(2500)
-        .deleteChars(13)
-        .typeString('Solopreneurs')
-        .pauseFor(2500)
-        .deleteChars(12)
-        .typeString("Startup-Founders")
-        .start()
+
+        typewriter
+            .typeString("Essential Tech Skills for Entrepreneurs")
+            .pauseFor(2500)
+            .deleteChars(13)
+            .typeString('Solopreneurs')
+            .pauseFor(2500)
+            .deleteChars(12)
+            .typeString("Startup-Founders")
+            .start();
+
 });
 
 
