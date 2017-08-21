c04b0f10333e6f1cbd9f179fe442e738562564f0
satenndrra <satenndrra@gmail.com>
2017-08-21 21:05:01 +0000

    styles for dark mode

--- scripts/main.js
+++ scripts/main.js
@@ -4,9 +4,10 @@
 
 $(function(){
 
-    var typed = document.getElementById('typed');
+    var typedBig= document.getElementById('typed-big');   // typewriter output for big screens
+    var typedSmall = document.getElementById('typed-small'); // typewriter output for smaller screens
     
-    var typewriter = new Typewriter(typed, {
+    var typewriter = new Typewriter(typedBig, {
         loop: true,
         blinkSpeed: 10,
         typingSpeed: 200
@@ -23,7 +24,24 @@ $(function(){
             .typeString("Own it.")
             .pauseFor(4000)                        
             .start();
+    
+    var typewriter = new Typewriter(typedSmall, {
+        loop: true,
+        blinkSpeed: 10,
+        typingSpeed: 200
+    });
 
+        typewriter
+            .pauseFor(4000)         
+            .typeString("Code it...")
+            .pauseFor(4000)
+            .deleteChars(10)
+            .typeString('Launch it..')
+            .pauseFor(4000)
+            .deleteChars(11)
+            .typeString("Own it.")
+            .pauseFor(4000)                        
+            .start();
 });
 
 
